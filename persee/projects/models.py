b90dae9795f268107ecc7446c7f9baddf85410e7
sebastien morele <moreleseb@gmail.com>
2020-11-01 13:48:49 +0100

    project model update to add a global presentation and a start date

--- persee/projects/models.py
+++ persee/projects/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 
 class Project(models.Model):
+    """ A project is a startup definition. """
     title = models.CharField(max_length=125)
-    program = models.ForeignKey(
-        'program',
-        null=True,
-        on_delete=models.CASCADE
-    )
+    presentation = models.TextField(blank=True)
+    start_date = models.DateField(null=True)
 
     def __str__(self):
         return self.title
 
 
 class Program(models.Model):
+    """ Programs are a specification of grand nancy innovation, a french incubator. Each mentored project is related
+    to a dedicated program. """
     title = models.CharField(max_length=125)
 
     def __str__(self):
