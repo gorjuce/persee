80b96d71bce3960bc4079b993dd79bccc562b816
sebastien morele <moreleseb@gmail.com>
2020-11-01 11:46:34 +0100

    add project and program management

--- persee/projects/models.py
+++ persee/projects/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=125)
+    program = models.ForeignKey(
+        'program',
+        null=True,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Program(models.Model):
+    title = models.CharField(max_length=125)
+
+    def __str__(self):
+        return self.title
