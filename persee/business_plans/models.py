5b1c7b69916467df49f2fe6345bb28508d89bd02
smorele <contact@sebastien-morele.fr>
2020-11-02 07:32:38 +0100

    doc

--- persee/business_plans/models.py
+++ persee/business_plans/models.py
@@ -4,9 +4,12 @@ from projects.models import Project
 
 
 class BusinessPlan(models.Model):
-    """ A business plan synthesizes all informations about a business from general strategy to financial overview """
+    """ A business plan synthesizes all information about a business from general strategy to financial overview """
     project = models.ForeignKey(
         Project,
         on_delete=models.CASCADE
     )
+
+    def __str__(self):
+        return "Business plan of: %s" % self.project
 
